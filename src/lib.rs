1a04f1b361c3295d8191711005de29fdb1504e70
dj8yf0μl <noreply@nowhere.org>
2025-05-19 13:41:06 +0300

    non-affectual change in key of a unit test assertion

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,6 @@ mod tests {
     #[test]
     fn get_nonexistent_message() {
         let contract = StatusMessage::default();
-        assert_eq!(None, contract.get_status("francis.near".parse().unwrap()));
+        assert_eq!(None, contract.get_status("francis-leo.near".parse().unwrap()));
     }
 }
